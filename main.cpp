3e259a3e4e7f8eb904f7d429c2e3c4f749de2f30
Jack Curtis <jack.curtis@kcl.ac.uk>
2021-09-25 00:17:35 +0100

    Added helloWord function

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+void helloWorld(){
+    std::cout << "Hello World!\n";
 }
+
+int main() {
+    helloWorld();
+}
+
+
